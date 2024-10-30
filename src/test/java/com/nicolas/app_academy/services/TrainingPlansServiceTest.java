15de05bb9400f4b012837d5f0dc9d00c1e3e678e
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-10-29 21:02:00 -0300

    Docs: added new remote git

--- src/test/java/com/nicolas/app_academy/services/TrainingPlansServiceTest.java
+++ src/test/java/com/nicolas/app_academy/services/TrainingPlansServiceTest.java
@@ -1,149 +1,151 @@
 package com.nicolas.app_academy.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+import com.nicolas.app_academy.dto.ExerciseDTO;
 import com.nicolas.app_academy.dto.TrainingPlansDTO;
+import com.nicolas.app_academy.entities.Exercise;
 import com.nicolas.app_academy.entities.TrainingPlans;
 import com.nicolas.app_academy.entities.User;
 import com.nicolas.app_academy.entities.enums.planDifficultyStatus;
+import com.nicolas.app_academy.repositories.ExerciseRepository;
 import com.nicolas.app_academy.repositories.TrainingPlansRepository;
 import com.nicolas.app_academy.repositories.UserRepository;
 import com.nicolas.app_academy.services.exception.ResourceNotFoundException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
 @SpringBootTest
-public class TrainingPlansServiceTest {
+class TrainingPlansServiceTest {
 
-    @Autowired
+    @InjectMocks
     private TrainingPlansService trainingPlansService;
 
-    @MockBean
+    @Mock
     private TrainingPlansRepository trainingPlansRepository;
 
-    @MockBean
+    @Mock
+    private ExerciseRepository exerciseRepository;
+
+    @Mock
     private UserRepository userRepository;
 
-    private TrainingPlans trainingPlan;
-    private TrainingPlansDTO trainingPlanDTO;
     private User user;
+    private TrainingPlans trainingPlans;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         user = new User();
-        user.setId(1L);
-        user.setName("Usuário Teste");
+        user.setName("John Doe");
         user.setTrainingPlans(new ArrayList<>());
 
-        trainingPlan = new TrainingPlans();
-        trainingPlan.setPlanName("Plano de Treino Teste");
-        trainingPlan.setPlanDescription("Descrição do Plano de Treino Teste");
-        trainingPlan.setDifficultyStatus(planDifficultyStatus.BEGINNER);
+        trainingPlans = new TrainingPlans();
+        trainingPlans.setPlanName("Full Body Workout");
+        trainingPlans.setPlanDescription("A comprehensive workout plan.");
+        trainingPlans.setDifficultyStatus(planDifficultyStatus.INTERMEDIATE);
+    }
 
-        trainingPlanDTO = new TrainingPlansDTO();
-        trainingPlanDTO.setPlanName("Plano de Treino Teste");
-        trainingPlanDTO.setPlanDescription("Descrição do Plano de Treino Teste");
+    @Test
+    void testCriarPlano_Success() {
+        // Prepare data
+        List<Long> userIds = List.of(1L);
+        ExerciseDTO exerciseDTO = new ExerciseDTO();
+        exerciseDTO.setExerciseName("Squat");
+        exerciseDTO.setExerciseDescription("Squat exercise");
+        exerciseDTO.setSeriesQuantity(3);
+        exerciseDTO.setRepetitionsQuantity(10);
+        exerciseDTO.setWeightUsed(70);
+        exerciseDTO.setRestTime(60);
+
+        TrainingPlansDTO trainingPlanDTO = new TrainingPlansDTO();
+        trainingPlanDTO.setPlanName("Plan 1");
+        trainingPlanDTO.setPlanDescription("Description 1");
+        trainingPlanDTO.setDifficultyStatus(planDifficultyStatus.INTERMEDIATE);
+        trainingPlanDTO.setNewExercises(List.of(exerciseDTO));
+        trainingPlanDTO.setExerciseIds(List.of(1L));
+
+        when(userRepository.findAllById(anyList())).thenReturn(List.of(user));
+        when(exerciseRepository.findById(1L)).thenReturn(Optional.of(new Exercise()));
+        when(trainingPlansRepository.save(any())).thenReturn(trainingPlans);
+
+        TrainingPlansDTO result = trainingPlansService.criarPlano(trainingPlanDTO, userIds);
+
+        assertNotNull(result);
+        assertEquals("Full Body Workout", result.getPlanName());
+        verify(userRepository, times(1)).save(any(User.class));
+        verify(exerciseRepository, times(1)).saveAll(anyList());
+    }
+
+    @Test
+    void testCriarPlano_UserNotFound() {
+        List<Long> userIds = List.of(1L);
+        TrainingPlansDTO trainingPlanDTO = new TrainingPlansDTO();
+
+        when(userRepository.findAllById(anyList())).thenReturn(new ArrayList<>());
+
+        assertThrows(ResourceNotFoundException.class, () -> trainingPlansService.criarPlano(trainingPlanDTO, userIds));
+    }
+
+    @Test
+    void testAtualizarPlano_Success() {
+        Long trainingPlanId = 1L;
+        TrainingPlansDTO trainingPlanDTO = new TrainingPlansDTO();
+        trainingPlanDTO.setPlanName("Updated Plan");
+        trainingPlanDTO.setPlanDescription("Updated Description");
         trainingPlanDTO.setDifficultyStatus(planDifficultyStatus.BEGINNER);
-        trainingPlanDTO.setExerciseIds(Collections.emptyList());
-        trainingPlanDTO.setNewExercises(Collections.emptyList());
-    }
 
-    @Test
-    public void shouldCriarPlano() {
-        when(userRepository.findAllById(Collections.singletonList(1L))).thenReturn(Collections.singletonList(user));
-        when(trainingPlansRepository.save(any(TrainingPlans.class))).thenReturn(trainingPlan);
+        when(trainingPlansRepository.findById(trainingPlanId)).thenReturn(Optional.of(trainingPlans));
+        when(trainingPlansRepository.save(any())).thenReturn(trainingPlans);
 
-        TrainingPlansDTO result = trainingPlansService.criarPlano(trainingPlanDTO, Collections.singletonList(1L));
+        TrainingPlansDTO result = trainingPlansService.atualizarPlano(trainingPlanId, trainingPlanDTO);
 
         assertNotNull(result);
-        assertEquals(trainingPlanDTO.getPlanName(), result.getPlanName());
-    }
-
-    @Test
-    public void shouldThrowResourceNotFoundExceptionQuandoUsuarioNaoEncontrado() {
-        when(userRepository.findAllById(Collections.singletonList(1L))).thenReturn(Collections.emptyList());
-
-        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () ->
-                trainingPlansService.criarPlano(trainingPlanDTO, Collections.singletonList(1L)));
-
-        assertEquals("Um ou mais usuários não foram encontrados.", exception.getMessage());
-    }
-
-    @Test
-    public void shouldThrowIllegalArgumentExceptionQuandoUsuarioTemMaisDeTresPlanosAtivos() {
-        User userWithPlans = new User();
-        userWithPlans.setId(2L);
-        userWithPlans.setTrainingPlans(new ArrayList<>(Collections.nCopies(3, trainingPlan)));
-
-        when(userRepository.findAllById(Collections.singletonList(2L))).thenReturn(Collections.singletonList(userWithPlans));
-
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                trainingPlansService.criarPlano(trainingPlanDTO, Collections.singletonList(2L)));
-
-        assertEquals("Usuario " + userWithPlans.getName() + " ja possui 3 planos de treino ativos.", exception.getMessage());
-    }
-
-    @Test
-    public void shouldListarPlanos() {
-        when(trainingPlansRepository.findAll()).thenReturn(Collections.singletonList(trainingPlan));
-
-        List<TrainingPlansDTO> result = trainingPlansService.listarPlanos();
-
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        assertEquals(trainingPlanDTO.getPlanName(), result.get(0).getPlanName());
-        verify(trainingPlansRepository, times(1)).findAll();
-    }
-
-    @Test
-    public void shouldAtualizarPlano() {
-        when(trainingPlansRepository.findById(1L)).thenReturn(Optional.of(trainingPlan));
-        when(trainingPlansRepository.save(any(TrainingPlans.class))).thenReturn(trainingPlan);
-
-        TrainingPlansDTO result = trainingPlansService.atualizarPlano(1L, trainingPlanDTO);
-
-        assertNotNull(result);
-        assertEquals(trainingPlanDTO.getPlanName(), result.getPlanName());
-        verify(trainingPlansRepository, times(1)).findById(1L);
+        assertEquals("Updated Plan", result.getPlanName());
         verify(trainingPlansRepository, times(1)).save(any(TrainingPlans.class));
     }
 
     @Test
-    public void shouldThrowResourceNotFoundExceptionQuandoPlanoNaoEncontradoParaAtualizar() {
-        when(trainingPlansRepository.findById(1L)).thenReturn(Optional.empty());
+    void testAtualizarPlano_NotFound() {
+        Long trainingPlanId = 1L;
+        TrainingPlansDTO trainingPlanDTO = new TrainingPlansDTO();
 
-        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () ->
-                trainingPlansService.atualizarPlano(1L, trainingPlanDTO));
+        when(trainingPlansRepository.findById(anyLong())).thenReturn(Optional.empty());
 
-        assertEquals("Plano de treino nao encontrado", exception.getMessage());
+        assertThrows(ResourceNotFoundException.class,
+                () -> trainingPlansService.atualizarPlano(trainingPlanId, trainingPlanDTO));
     }
 
     @Test
-    public void shouldDeletarPlano() {
-        when(trainingPlansRepository.existsById(1L)).thenReturn(true);
+    void testDeletarPlano_Success() {
+        Long trainingPlanId = 1L;
 
-        trainingPlansService.deletarPlano(1L);
+        when(trainingPlansRepository.existsById(trainingPlanId)).thenReturn(true);
 
-        verify(trainingPlansRepository, times(1)).deleteById(1L);
+        trainingPlansService.deletarPlano(trainingPlanId);
+
+        verify(trainingPlansRepository, times(1)).deleteById(trainingPlanId);
     }
 
     @Test
-    public void shouldThrowResourceNotFoundExceptionQuandoPlanoNaoEncontradoParaDeletar() {
-        when(trainingPlansRepository.existsById(1L)).thenReturn(false);
+    void testDeletarPlano_NotFound() {
+        Long trainingPlanId = 1L;
 
-        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () ->
-                trainingPlansService.deletarPlano(1L));
+        when(trainingPlansRepository.existsById(trainingPlanId)).thenReturn(false);
 
-        assertEquals("Plano de treino nao encontrado", exception.getMessage());
+        assertThrows(ResourceNotFoundException.class, () -> trainingPlansService.deletarPlano(trainingPlanId));
     }
 }
