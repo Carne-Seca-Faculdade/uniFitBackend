4eab51bfbba451effa8a48d2351bf68f01aa7d6b
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-11-04 21:11:17 -0300

    refactor: added new features

--- src/main/java/com/nicolas/app_academy/repositories/ExerciseLogRepository.java
+++ src/main/java/com/nicolas/app_academy/repositories/ExerciseLogRepository.java
@@ -14,18 +14,18 @@ import com.nicolas.app_academy.entities.User;
 
 @Repository
 public interface ExerciseLogRepository extends JpaRepository<ExerciseLog, Long> {
-  List<ExerciseLog> findByUser(User user);
+    List<ExerciseLog> findByUser(User user);
 
-  List<ExerciseLog> findByExercise(Exercise exercise);
+    List<ExerciseLog> findByExercise(Exercise exercise);
 
-  List<ExerciseLog> findByUserAndStatus(User user, String status);
+    List<ExerciseLog> findByUserAndStatus(User user, String status);
 
-  Long countByUserIdAndStatus(Long userId, String status);
+    Long countByUserIdAndStatus(Long userId, String status);
 
-  @Query("SELECT COUNT(e) FROM ExerciseLog e WHERE e.user.id = :userId AND e.status = 'Concluido' " +
-      "AND e.performedAt >= :startDate AND e.performedAt <= :endDate")
-  Long countConcluidoExercisesByUserAndDateRange(
-      @Param("userId") Long userId,
-      @Param("startDate") LocalDateTime startDate,
-      @Param("endDate") LocalDateTime endDate);
+    @Query("SELECT COUNT(e) FROM ExerciseLog e WHERE e.user.id = :userId AND e.status = 'Concluido' " +
+            "AND e.performedAt >= :startDate AND e.performedAt <= :endDate")
+    Long countConcluidoExercisesByUserAndDateRange(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);
 }
